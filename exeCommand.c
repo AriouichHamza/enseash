ad7fd332e01e47cc90772c020fe21a67a5b65b98
AriouichHamza <147997826+AriouichHamza@users.noreply.github.com>
2023-11-27 11:31:20 +0100

    Update exeCommand.c

--- exeCommand.c
+++ exeCommand.c
@@ -1,24 +1,38 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <wait.h>
+#include <sys/wait.h>
 
-#include "exeCommand.h"
-
+// Function to execute a command using fork and execlp
+// Parameters:
+//   - command: The command to be executed
+//   - command_size: Size of the command string
+// Returns:
+//   - 0 on success, -1 on failure
 int exeCommand(char *command, ssize_t command_size) {
+    // Fork the process to create a child process
     int pid = fork();
     int status;
 
     if (pid == 0) {
         // Child process
+        // Execute the command using execlp
         execlp(command, command, NULL);
-        exit(EXIT_SUCCESS);
+
+        // If execlp fails, exit the child process with failure status
+        exit(EXIT_FAILURE);
 
     } else {
         // Parent process
-        wait(NULL);
+        // Wait for the child process to complete
+        wait(&status);
+
+        // Check if the child process exited normally
+        if (WIFEXITED(status)) {
+            // Return the exit status of the child process
+            return WEXITSTATUS(status);
+        } else {
+            // Return -1 to indicate failure (child process did not exit normally)
+            return -1;
+        }
     }
-    return 0;
 }
